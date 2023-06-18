4684e1ab4996ccc83f184e6c0d6265899f7a32b0
0Axolote <129017609+0Axolote@users.noreply.github.com>
2023-06-18 01:21:04 -0600

    Update insert_product.php

--- insert_product.php
+++ insert_product.php
@@ -2,26 +2,57 @@
 include("connection.php");
 $con = connection();
 
+$id = null;
 $nombre = $_POST['nombre'];
 $marca = $_POST['marca'];
 $descripcion = $_POST['descripcion'];
 $costo = $_POST['costo'];
 $stock = $_POST['stock'];
 
-/*    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    nombre VARCHAR(50),
-    marca VARCHAR(150),
-    descripcion TEXT,
-    costo DECIMAL(10,2)
-    stock ENUM ('disponible', 'agotado')*/
+// Validación de datos
+$errors = array();
 
-$sql = "INSERT INTO Producto VALUES('$nombre','$marca','$descripcion','$costo','$stock')";
-$query = mysqli_query($con, $sql);
-
-if($query){
-    Header("Location: ./productos.php?#Crud");
-}else{
-
+if (empty($nombre)) {
+    $errors['nombre'] = "El campo 'Nombre' es requerido.";
 }
 
+if (empty($marca)) {
+    $errors['marca'] = "El campo 'Marca' es requerido.";
+}
+
+if (empty($descripcion)) {
+    $errors['descripcion'] = "El campo 'Descripción' es requerido.";
+}
+
+if (empty($costo)) {
+    $errors['costo'] = "El campo 'Costo' es requerido.";
+} elseif (!is_numeric($costo)) {
+    $errors['costo'] = "El campo 'Costo' debe ser un número válido.";
+}
+
+if (empty($stock)) {
+    $errors['stock'] = "El campo 'Stock' es requerido.";
+}
+
+// Verificar si hay errores de validación
+if (!empty($errors)) {
+    // Convertir el array de errores a una cadena de texto en formato JSON
+    $errors_json = json_encode($errors);
+    // Redirigir de vuelta al formulario con los mensajes de error como parámetros en la URL
+    header("Location: productos.php?errors=$errors_json");
+    exit();
+}
+
+// Realizar la inserción en la base de datos
+$sql = "INSERT INTO producto VALUES('$id','$nombre','$marca','$descripcion','$costo','$stock')";
+$query = mysqli_query($con, $sql);
+
+if ($query) {
+    // Redirigir a la página de productos con un mensaje de éxito
+    header("Location: productos.php?success=Producto agregado correctamente");
+} else {
+    // Error en la consulta SQL
+    // Redirigir a la página de productos con un mensaje de error
+    header("Location: productos.php?error=Error al agregar el producto");
+}
 ?>
