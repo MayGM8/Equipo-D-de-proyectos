f3cbcdc5d2fa3077581150f8f26ff49db6895da1
0Axolote <129017609+0Axolote@users.noreply.github.com>
2023-06-18 01:28:40 -0600

    Update edit_product.php

--- edit_product.php
+++ edit_product.php
@@ -1,5 +1,4 @@
 <?php
-
 include("connection.php");
 $con = connection();
 
@@ -10,14 +9,42 @@ $descripcion = $_POST['descripcion'];
 $costo = $_POST['costo'];
 $stock = $_POST['stock'];
 
+$errors = array(); // Arreglo para almacenar los mensajes de error
 
-$sql="UPDATE producto SET nombre='$nombre', marca='$marca', descripcion='$descripcion', costo='$costo', stock='$stock' WHERE id='$id'";
-$query = mysqli_query($con, $sql);
+if (empty($nombre)) {
+    $errors['nombre'] = "El campo 'Nombre' es obligatorio.";
+}
 
-if($query){
-    Header("Location: productos.php");
-}else{
+if (empty($marca)) {
+    $errors['marca'] = "El campo 'Marca' es obligatorio.";
+}
 
+if (empty($descripcion)) {
+    $errors['descripcion'] = "El campo 'Descripción' es obligatorio.";
+}
+
+if (empty($costo)) {
+    $errors['costo'] = "El campo 'Costo' es obligatorio.";
+}
+
+if (empty($stock)) {
+    $errors['stock'] = "Debe seleccionar una opción para 'Stock'.";
+}
+
+if (count($errors) === 0) {
+    // No hay errores, realizar la actualización en la base de datos
+    $sql = "UPDATE producto SET nombre='$nombre', marca='$marca', descripcion='$descripcion', costo='$costo', stock='$stock' WHERE id='$id'";
+    $query = mysqli_query($con, $sql);
+
+    if ($query) {
+        Header("Location: productos.php");
+    } else {
+        // Error al ejecutar la consulta
+    }
+} else {
+    // Redireccionar a update.php con los errores en la URL
+    $errorsParam = urlencode(json_encode($errors));
+    Header("Location: update.php?id=$id&errors=$errorsParam");
 }
 
 ?>
